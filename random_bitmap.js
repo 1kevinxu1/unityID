6db68cd9cb0dd1509e4304e1594822ccc2965441
Kevin Xu <1kevinxu1@gmail.com>
2017-04-01 19:45:09 -0400

    build query & make request after testing

--- random_bitmap.js
+++ random_bitmap.js
@@ -1,11 +1,25 @@
-require 'net/http'
+var request = require('request');
+var baseApi = 'https://www.random.org/integers'
+var params  = {
+  "num": 6,
+  "min": 0,
+  "max": 255,
+  "base": 10,
+  "col": 2,
+  "format": "plain",
+  "rnd": "new"
+}
 
-class RandomClient
+function buildQuery(params) {
+  var queryString = "/?";
+  for (var key in params){
+    queryString += key + "=" + params[key] + "&"
+  };
+  return baseApi + queryString
+};
 
-  def initialize
-    
-  end
-
-end
-
-Net::HTTP.get()
+console.log(buildQuery(params))
+request(buildQuery(params), function(error, response, body) {
+  console.log(response);
+  console.log(body);
+});
